1d245e13a0e2bca4cdddd7abf584bdfc47d14bdf
risoee <rinisoe@hawaii.edu>
2026-09-02 12:57:27 -1000

    excersice 2

--- Lab 2/Ex1.py
+++ Lab 2/Ex1.py
@@ -1,3 +1,7 @@
+# This program prompts the user to enter a number between 1 and 100, converts the input to an integer, calculates the square of that number, and then prints both the original number and its square.   
+# number, and then prints both the entred number and its sqaure. 
+# Name: Rin Isoe 
+# Date: Sept. 2, 2026
 value_entered = input("Enter a number between 1 and 100:")
 value_as_integer = int(value_entered)
 
@@ -5,3 +9,5 @@ valueSquared = value_as_integer ** 2
 
 print("You entered:", value_as_integer)
 print("The square of the number you entered is:", valueSquared)
+
+print(f"You entered: {value_as_integer}, and the square of {value_as_integer} is: {valueSquared}")
---
+++ Lab 2/Ex2.py
@@ -0,0 +1,10 @@
+# Ask the user to enter their birth year. Calculate their age based the current year and print it out. 
+# Name: Rin Isoe 
+# Date: Sept. 2, 2026
+
+birth_year = input("Enter your birth year:")
+current_year = 2026
+age = current_year - int(birth_year)
+
+print("You entered:", birth_year)
+print("Your age is:", age)
