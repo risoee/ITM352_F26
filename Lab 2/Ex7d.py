9df2ff04f08d9d0bf238d0db4f8b54163b57437a
risoee <rinisoe@hawaii.edu>
2026-09-04 12:49:45 -1000

    ex 7 and 7d

--- Lab 2/Ex7d.py
+++ Lab 2/Ex7d.py
@@ -1,13 +1,18 @@
 # This program will ask the user to enter a temperature in Fahrenheit and then convert it to Celsius and print out the result.
+# Create the conversion as a function. 
 # Name: Rin Isoe
 # Date: Sept. 4, 2026
+
+def F_to_C(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9
+    rounded_celsius = round(celsius, 2)
+    return rounded_celsius
+
 
 fahrenheit_input = input("Enter a temperature in Fahrenheit: ")
 fahrenheit_float = float(fahrenheit_input)
 
-celsius_value = (fahrenheit_float - 32) * 5 / 9
-
-celcius_value = round(celsius_value, 2)
+celsius_value = F_to_C(fahrenheit_float)
 
 print("You entered:", fahrenheit_float)
 print("The temperature in Celsius is:", celsius_value)
