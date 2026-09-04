553a7f2de3b1329eff6e54f738a9e5354641c43f
risoee <rinisoe@hawaii.edu>
2026-09-04 12:16:37 -1000

    Create Ex5.py

---
+++ Lab 2/Ex5.py
@@ -0,0 +1,8 @@
+#This program will ask the user to enter a string and then print out the string and its length.
+# Name: Rin Isoe
+# Date: Sept. 4, 2026 
+user_string = input(" Please enter a string:")
+string_length = len(user_string)
+
+print("You entered:", user_string)
+print("The length of the string you entered is:", string_length)
